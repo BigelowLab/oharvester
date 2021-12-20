51f36b3448442ac6d0797d99bb20d4d3c4913ec6
Johnathan Evanilla <jevanilla@bigelow.org>
2021-12-20 10:59:53 -0500

    Added NAO script

--- scripts/NAO-timeseries/get_nao.R
+++ scripts/NAO-timeseries/get_nao.R
@@ -3,28 +3,19 @@ library(readr)
 library(dplyr)
 library(tidyr)
 
-### https://psl.noaa.gov/data/timeseries/AMO/
-amo <- readLines("https://psl.noaa.gov/data/correlation/amon.us.long.data") 
+### https://www.cpc.ncep.noaa.gov/products/precip/CWlink/pna/nao.shtml
+nao <- readLines("https://www.cpc.ncep.noaa.gov/products/precip/CWlink/pna/norm.nao.monthly.b5001.current.ascii.table")
 
-dims <- amo[1] %>%
-  stringr::str_trim(side = "both") %>%
-  stringr::str_squish() %>%
-  stringr::str_split(pattern = " ") %>%
-  `[[`(1) %>%
-  as.numeric()
 
-n <- dims[2]-dims[1] + 2
-
-wide_form <- amo[2:n] %>%
+wide_form <- nao  %>%
   stringr::str_trim(side = "both") %>%
   stringr::str_squish() %>%
   stringr::str_replace_all(stringr::fixed(" "), stringr::fixed(",")) %>%
   paste(collapse = "\n") %>%
   readr::read_csv(col_names = c("Year", month.abb),
-                  col_types = strrep("d", 13)) 
+                  col_types = strrep("d", 13))
 
 long_form <- wide_form %>%
   tidyr::pivot_longer(all_of(month.abb), 
                       names_to = "Month",
                       values_to = "AMO")
-  
